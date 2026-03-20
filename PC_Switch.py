03527ecd4f59fe20219c5167e91db0d55829579d
HomelessSpud <ethan.c.venters@gmail.com>
2026-03-20 15:04:30 +0000

    Add HTML interface for remote PC power control

--- PC_Switch.py
+++ PC_Switch.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, send_from_directory
 import RPi.GPIO as GPIO
 import time
+import os
 
 POWER_PIN = 17
 
@@ -8,16 +9,21 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(POWER_PIN, GPIO.OUT, initial=GPIO.LOW)
 
 def press_power():
+    print("POWER PRESSED")
     GPIO.output(POWER_PIN, GPIO.HIGH)
-    time.sleep(0.2)
+    time.sleep(1)
     GPIO.output(POWER_PIN, GPIO.LOW)
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return send_from_directory("/home/ethan/Documents/Server/Home-Server-PC-Switch/", "index.html")
+
 @app.route("/api/power", methods=["POST"])
 def power():
     token = request.headers.get("X-Auth")
-    if token != "YOUR_SECRET_TOKEN":
+    if token != "Event":
         abort(403)
     press_power()
     return "OK"
